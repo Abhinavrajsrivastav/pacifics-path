2b8e351eaea64bfc72331689e46eba623028871f
Abhinav Raj Srivastava <abhiraj.srivast254@gmail.com>
2024-05-02 23:34:03 +0530

    first commit

---
+++ edubell/src/main/java/com/edubbell/edubell/EdubellApplication.java
@@ -0,0 +1,13 @@
+package com.edubbell.edubell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdubellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdubellApplication.class, args);
+	}
+
+}
---
+++ edubell/src/test/java/com/edubbell/edubell/EdubellApplicationTests.java
@@ -0,0 +1,14 @@
+package com.edubbell.edubell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdubellApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+	}
+
+}
