62d1d812fde2b578c13c8958c416da036fad691c
Abhinav Raj Srivastava <abhiraj.srivast254@gmail.com>
2024-06-23 02:14:32 +0530

    23-06-2024-004

--- src/Components/Welcome/Pages/Featues-Page/Features.jsx
+++ src/Components/Welcome/Pages/Featues-Page/Features.jsx
@@ -18,12 +18,12 @@ function Features() {
         <span className="Features-Page-SubHeading">Explore our amazing features</span>
       </h1>
       <div className="features-body">
-        <FeaturesCard className="features-card" icons={"./Icons/shield.png"} heading={"Secure Environment"} subheading={"A place where you do authentic, relevance and secure knowledge sharing."}/>
-        <FeaturesCard className="features-card" icons={"./Icons/aisecure.png"} heading={"AI Monitored"} subheading={"A place where AI will be protector of your chat, resources, and services."}/>
-        <FeaturesCard className="features-card" icons={"./Icons/video-camera.png"} heading={"Group study, video call"} subheading={"A place where you can connect, share and learn together on common topics."}/>
-        <FeaturesCard className="features-card" icons={"./Icons/send.png"} heading={"Share your learning"} subheading={"A place where you can share your learning and knowledge with your peers."}/>
-        <FeaturesCard className="features-card" icons={"./Icons/idea.png"} heading={"Share your idea"} subheading={"A place where you can share your ideas openly."}/>
-        <FeaturesCard className="features-card" icons={"./Icons/chat.png"} heading={"Secure and private chat"} subheading={"A place where you can communicate, share, talk at common points in a secure environment."}/>
+        <FeaturesCard className="features-card" icons={"./Icons/shield.png"} heading={"Read Books"} subheading={"A place where you do authentic, relevance and secure knowledge sharing."}/>
+        <FeaturesCard className="features-card" icons={"./Icons/aisecure.png"} heading={"Find Right Mate"} subheading={"A place where AI will be protector of your chat, resources, and services."}/>
+        <FeaturesCard className="features-card" icons={"./Icons/video-camera.png"} heading={"See your Ranking"} subheading={"A place where you can connect, share and learn together on common topics."}/>
+        <FeaturesCard className="features-card" icons={"./Icons/send.png"} heading={"Learn with Ai"} subheading={"A place where you can share your learning and knowledge with your peers."}/>
+        <FeaturesCard className="features-card" icons={"./Icons/idea.png"} heading={"Learn With community"} subheading={"A place where you can share your ideas openly."}/>
+        <FeaturesCard className="features-card" icons={"./Icons/chat.png"} heading={"Ask with Gemini"} subheading={"A place where you can communicate, share, talk at common points in a secure environment."}/>
       </div>
     </div>
   );
