fafdd8d567776f393e47901e0231974ecddb2d1b
Abhinav Raj Srivastava <abhiraj.srivast254@gmail.com>
2024-07-28 11:08:44 +0530

    28-07-2024-001

--- src/Components/Welcome/Pages/Working-Features/Developers/Developers.jsx
+++ src/Components/Welcome/Pages/Working-Features/Developers/Developers.jsx
@@ -11,7 +11,7 @@ function Developers() {
         </div>
         <div className="about-developer">
         <h2>Abhinav Raj Srivastava</h2>
-        <p>Hey! Developers, I am Abhinav Raj Srivastav. Currently a Final Year Student at ABES Engineering College Ghaziabad. I would be very happy to listen any valuable feedback from your side, feel free you write it in the feedback section. As always, Keep Hustling 🤌🚀🧑‍🚀.</p>
+        <p>Hey! Developers, I am Abhinav Raj Srivastav. Currently a Final Year Student at ABES Engineering College Ghaziabad. I would be very happy to listen any valuable feedback from your side, feel free to write it in the feedback section. As always, Keep Hustling 🤌🚀🧑‍🚀.</p>
           </div>
       </div>
     </div>
