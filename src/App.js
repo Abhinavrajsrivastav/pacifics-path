dedf047cf5cf9958590c8a7bf9eb3c55ec61eabc
Abhinav Raj Srivastava <abhiraj.srivast254@gmail.com>
2024-10-21 16:57:09 +0530

    21-10-2024-001

--- src/App.js
+++ src/App.js
@@ -30,44 +30,46 @@ import LeaderBoard from './Components/Welcome/Pages/Working-Features/LearderBoar
 import Feedback from './Components/Welcome/Feedback/Feedback';
 import CoverLetterGenerator from './Pages/CoverLetter/CoverLetterGenerator';
 import Community from './Pages/Community/Community';
+import Chatbot from './Components/IBM_Service/Chatbot';
 
 function App() {
   return (
-    <Router>
-      <AuthProvider>
-        <Navbar />
-        <Routes>
-          <Route path="/pacifics-path" element={
-            <>
-              <Vision />
-              <Features />
-              <Testimonials />
-              <Developers />
-              <FAQ />
-              {/* <Feedback /> */}
-            </>
-          } />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/categories" element={<Categories />} />
-          <Route path="/selfLearn" element={<SelfLearn />} />
-          <Route path="/self-learn/response" element={<Result />} />
-          <Route path="/self-learn/response/Videos" element={<Videos />} />
-          <Route path="/read-books" element={<Books />} />
-          <Route path="/github-profile" element={<GitHubProfile />} />
-          <Route path="/git-mate" element={<Mate />} />
-          <Route path="/chat/:topic" element={<ChatRoom />} />
-          <Route path="/Compare" element={<Compare />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/learderboard" element={<LeaderBoard />} />
-          <Route path="/feedback" element={<Feedback />} />
-          <Route path="/cover-letter" element={<CoverLetterGenerator />} />
-          <Route path="/join-community" element={<Community />} />
-        </Routes>
-        <Footer />
-      </AuthProvider>
-    </Router>
+    // <Router>
+    //   <AuthProvider>
+    //     <Navbar />
+    //     <Routes>
+    //       <Route path="/pacifics-path" element={
+    //         <>
+    //           <Vision />
+    //           <Features />
+    //           <Testimonials />
+    //           <Developers />
+    //           <FAQ />
+    //           {/* <Feedback /> */}
+    //         </>
+    //       } />
+    //       <Route path="/login" element={<Login />} />
+    //       <Route path="/signup" element={<Signup />} />
+    //       <Route path="/profile" element={<Profile />} />
+    //       <Route path="/watson-bot" element={<Chatbot />} />
+    //       <Route path="/categories" element={<Categories />} />
+    //       <Route path="/selfLearn" element={<SelfLearn />} />
+    //       <Route path="/self-learn/response" element={<Result />} />
+    //       <Route path="/self-learn/response/Videos" element={<Videos />} />
+    //       <Route path="/read-books" element={<Books />} />
+    //       <Route path="/github-profile" element={<GitHubProfile />} />
+    //       <Route path="/git-mate" element={<Mate />} />
+    //       <Route path="/chat/:topic" element={<ChatRoom />} />
+    //       <Route path="/Compare" element={<Compare />} />
+    //       <Route path="/projects" element={<Projects />} />
+    //       <Route path="/learderboard" element={<LeaderBoard />} />
+    //       <Route path="/feedback" element={<Feedback />} />
+    //       <Route path="/cover-letter" element={<CoverLetterGenerator />} />
+    //       <Route path="/join-community" element={<Community />} />
+    //     </Routes>
+    //     <Footer />
+    //   </AuthProvider>
+    // </Router>
   );
 }
 
